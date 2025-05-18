8d6d6da67cdee2c03a727771210c48c5b0526ba1
Muniyammal L <143873736+Muni28@users.noreply.github.com>
2025-05-18 23:58:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,5 +31,4 @@ def index():
     return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render dynamic port
-    app.run(host="0.0.0.0", port=port)        # Make app accessible
+    app.run(host='0.0.0.0', port=5000)
